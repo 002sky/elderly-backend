667047d1689db834224ec64e6935794ac6ac48a3
002sky <87744714+002sky@users.noreply.github.com>
2022-12-29 00:12:45 +0800

    update message box function

--- app/Http/Controllers/messageBoxController.php
+++ app/Http/Controllers/messageBoxController.php
@@ -14,27 +14,37 @@ class messageBoxController extends Controller
 
     public function getAllMessage(Request $request){
         $recipientId = 2;
-        $messages = MessageBox::where('receiver_ID', $recipientId)
+        $messages = MessageBox::where('receiver_ID', $recipientId)->orWhere('sender_ID', $recipientId)
             ->with('sender', 'receiver')
             ->get();
         
-        foreach ($messages as $message) {
+        foreach($messages as $messages){
 
-            echo $message->sender->name;
-            echo $message->receiver->name;
+            $JsonMessage[] = [
+                'senderID' => $messages->sender->id,
+                'receiverID' => $messages->receiver->id,
+                'message' =>$messages->message,
+            ];
+
+
         }
+
+
+
+        return response()->json(
+            [$JsonMessage]
+        );
+
     }
 
 
     public function getAllReceiver(Request $request){
 
-        if($request->userType == "true"){
+        if($request->userType == false){
             $receiverList = DB::table('users')->where('status','=','is_admin')->select('id','name')->get();
         }else{
             $receiverList = DB::table('users')->where('status','=','Relative')->select('id','name')->get();
         }
-
-
         return response()->json(
 
             $receiverList
