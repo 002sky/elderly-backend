af6c9e1289f496e7966244fd93c4af3cdaeac982
huilongtee <huilongtee0225@gmail.com>
2023-02-17 11:02:08 +0800

    login design changed but haven't complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,132 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center ">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <img src=".." alt="">
+                  <span class="d-none d-lg-block">WeCare</span>
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                    <p class="text-center small">Enter your username & password to login</p>
+                  </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                  <form class="row g-3 needs-validation" novalidate>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Username</label>
+                      <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                        <input type="text" name="username" class="form-control" id="yourUsername" required>
+                        <div class="invalid-feedback">Please enter your username.</div>
+                      </div>
+                    </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                    <div class="col-12">
+                      <label for="yourPassword" class="form-label">Password</label>
+                      <input type="password" name="password" class="form-control" id="yourPassword" required>
+                      <div class="invalid-feedback">Please enter your password!</div>
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <div class="col-12">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
+                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Login</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                    </div>
+                  </form>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
                 </div>
+              </div>
+
+              
+
             </div>
+          </div>
         </div>
+
+      </section>
+
     </div>
-</div> -->
-
-<div class="login-section">
-    <div class="layer"></div>
-    <div class="container">
-        <div class="top-section">
-            <h1 class="page-title">Login</h1>
-        </div>
-        <div class="left-section">
-            <!-- <img src="./images/logo.png" alt=""> -->
-            <img src="./images/WeCare-logos_black.png" alt="">
-
-        </div>
-        <div class="right-section">
-            <form>
-
-                <div class="form-row">
-                    <div class="input">
-                        <div class="icon">
-                            <i class="fas fa-user-circle"></i>
-                        </div>
-
-                        <input type="text" id="email" name="email" placeholder="Email">
-                    </div>
-                    <!--error message-->
-
-                    <!-- <div class="message">
-                        @error('email')
-                        <span class="" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div> -->
-                </div>
-                <div class="form-row">
-                    <div class="input">
-                        <div class="icon">
-                            <i class="fas fa-lock"></i>
-                        </div>
-                        <input type="password" id="password" name="password" placeholder="Password">
-                    </div>
-                    <!--error message-->
-                    <!-- <div class="message">
-                        @error('password')
-                        <span class="" role="">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div> -->
-                </div>
-                <div class="form-row">
-                    <button type="submit" class="btn-login">Login</button>
-                </div>
-                <!-- <div class="form-row">
-                    <span>Do not have an account? <a href="#">Click here</a> </span>
-                </div> -->
-            </form>
-        </div>
-    </div>
-</div>
+  </main><!-- End #main -->
 @endsection
